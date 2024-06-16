38a43981f1ba2ce6f5920ec2522ae9aaf1e00dd7
shunmaruko <t.shunryou@gmail.com>
2024-06-16 16:45:31 +0900

    DEV: setup linter as much as possible

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,130 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2023: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: [
+    'dist', 
+    '.eslintrc.cjs',
+    'node_modules/*',
+  ],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  plugins: [
+    'react', 
+    'react-refresh',
+    'import',
+  ],
+  rules: { //TODO: enforce react component to PascallCase
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    "no-console":[
+      "error",
+      {
+        "allow": ["warn", "error"]
+      },
+    ],
+    "no-else-return": "error",
+    'linebreak-style': ['error', 'unix'],
+    // TODO: set import/no-restricted-paths
+    'import/no-cycle': 'error',
+    'import/no-named-as-default-member': 'off',
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'parent',
+          'sibling',
+          'index',
+        ],
+        'newlines-between': 'always',
+        alphabetize: { order: 'asc', caseInsensitive: true },
+      },
+    ],
+    "react/function-component-definition": [
+      "error",
+      {
+        "namedComponents": "arrow-function",
+        "unnamedComponents": 'arrow-function'
+      }
+    ],
   },
+  overrides: [
+    { // ts/tsx specific rules
+      files: ['**/*.ts', '**/*.tsx'],
+      extends: ['plugin:@typescript-eslint/recommended'],
+      parser: '@typescript-eslint/parser',
+      settings: {
+        react: { version: 'detect' },
+      },
+      plugins: [
+        'check-file', 
+      ],
+      rules: {
+        '@typescript-eslint/no-unused-vars': ['error'],
+        "@typescript-eslint/naming-convention": [
+          "error",
+          {  // function or component
+            "selector": "function",
+            "format": ["camelCase", "PascalCase"],
+            "leadingUnderscore": "forbid",
+          },
+          {
+            "selector": "variable",
+            "format": ["strictCamelCase",'UPPER_CASE'],
+          },
+          {
+            "selector": "variable",
+            "format": ["strictCamelCase"],
+            "types": ["boolean"],
+            "prefix": ["is", "should"]
+          },
+          {
+            "selector": 'variable',
+            "modifiers": ['const', 'exported'],
+            "types": ['function'],
+            "format": ['StrictPascalCase'],
+          },
+          {
+            'selector': 'typeAlias',
+            'format': ['StrictPascalCase']
+          },
+          {
+            "selector": "import",
+            "format": ['camelCase', 'PascalCase'],
+          },
+        ],
+        'check-file/filename-naming-convention': [
+          'error',
+          {
+            '**/*.tsx': 'PASCAL_CASE',
+            '**/*.ts': 'KEBAB_CASE',
+          },
+          {
+            ignoreMiddleExtensions: true,
+          },
+        ],
+      }
+    },
+    {
+      plugins: ['check-file'],
+      files: ['src/**/!(__tests__)/*'], // exclude __tests__
+      rules: {
+        'check-file/folder-naming-convention': [
+          'error',
+          {
+            '**/*': 'KEBAB_CASE',
+          },
+        ],
+      },
+    },
+  ],
 }
