4b25754d3f43c22c5a1604bbac7bc41806f941d4
shunmaruko <t.shunryou@gmail.com>
2024-07-13 14:30:14 +0900

    DEV: introduce bind volume to docker-compose-dev

--- backend/api_auto_generated/src/openapi_server/impl/auth_impl.py
+++ backend/api_auto_generated/src/openapi_server/impl/auth_impl.py
@@ -4,7 +4,6 @@ from openapi_server.apis.auth_api_base import BaseAuthApi
 
 class AuthApiImpl(BaseAuthApi):
     def get_user_auth_me_get(self) -> User:
-        #print(User(email="hoge@gmail.com", roles=["ADMIN", "GENERAL"]))
         return User(email="hoge@gmail.com", roles=["ADMIN"])
     
     def login_auth_login_post(self, email: str, password: str) -> User:
